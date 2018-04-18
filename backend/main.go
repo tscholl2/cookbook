504660f85b3c6214cb8604ceb521ad0d4d568f19
Travis <tscholl2@uw.edu>
2018-04-18 11:15:36 -0700

    some small helper functions

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"compress/gzip"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/pkg/errors"
 )
+
+type Recipe struct {
+	Name        string
+	Directions  []string
+	Ingredients []string
+}
+
+func newRecipe(w http.ResponseWriter, r *http.Request) {
+	var R Recipe
+	if err := readJSON(r, &R); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	fmt.Println("got recipe: R", R)
+	w.WriteHeader(http.StatusOK)
+}
+
+func getRecipe(w http.ResponseWriter, r *http.Request) {
+	R := Recipe{Name: "", Directions: []string{}, Ingredients: []string{}}
+	if err := writeJSON(w, R); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
@@ -12,7 +38,26 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/get", getRecipe)
+	http.HandleFunc("/new", newRecipe)
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func writeJSON(w http.ResponseWriter, v interface{}) error {
+	w.Header().Set("content-type", "application/json;charset=utf-8")
+	w.Header().Set("content-encoding", "gzip")
+	W, err := gzip.NewWriterLevel(w, 1)
+	if err != nil {
+		return err
+	}
+	defer W.Close() // TODO handle error
+	encoder := json.NewEncoder(W)
+	encoder.SetIndent("", "  ")
+	return errors.Wrap(encoder.Encode(v), "json")
+}
+
+func readJSON(r *http.Request, v interface{}) error {
+	return errors.Wrap(json.NewDecoder(r.Body).Decode(v), "json")
 }
