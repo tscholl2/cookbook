cb37e8176937e8ce7db026a1884ea104039dbc00
Travis Scholl <traviswscholl@gmail.com>
2019-09-15 12:15:23 -0400

    stuck adding items to list...

--- src/view/notecard/index.tsx
+++ src/view/notecard/index.tsx
@@ -13,6 +13,12 @@ interface NotecardProps extends Recipe {
 
 export function Notecard(props: NotecardProps) {
   const { title = "", ingrediants = "", directions = "", onchange } = props;
+  let ctrlDown = false,
+    ctrlKey = 17,
+    cmdKey = 91,
+    // vKey = 86,
+    // cKey = 67,
+    sKey = 83;
   const handlers = {
     contenteditable: onchange != null,
     onfocusout(e: any) {
@@ -20,37 +26,63 @@ export function Notecard(props: NotecardProps) {
       while (div == null || !div.classList.contains("notecard")) {
         div = (div || e.target).parentElement;
       }
+      console.log("OLD DIRECTIONS: ", JSON.stringify(directions));
+      console.log(
+        "NEW DIRECTIONS: ",
+        JSON.stringify(
+          div
+            .getElementsByTagName("ol")!
+            .item(0)!
+            .innerText.split("\n")
+            .map(s => s.trim())
+            .filter(s => s !== "")
+            .join("\n")
+        )
+      );
       onchange!({
         title: div.getElementsByTagName("h1")!.item(0)!.innerText,
-        ingrediants: div.getElementsByTagName("ul")!.item(0)!.innerText,
-        directions: div.getElementsByTagName("ol")!.item(0)!.innerText
+        ingrediants: div
+          .getElementsByTagName("ul")!
+          .item(0)!
+          .innerText.split("\n")
+          .map(s => s.trim())
+          .filter(s => s !== "")
+          .join("\n"),
+        directions: div
+          .getElementsByTagName("ol")!
+          .item(0)!
+          .innerText.split("\n")
+          .map(s => s.trim())
+          .filter(s => s !== "")
+          .join("\n")
       });
     },
     onkeydown(e: any) {
-      if (false && e.which === 13) {
+      if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+      if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+      if (ctrlDown && e.keyCode == sKey) {
+        console.log("save found!");
         e.preventDefault();
-        // TODO: save
-        console.log("saving...");
       }
     }
   };
+  console.log(
+    "ABOUT TO RENDER STR: ",
+    JSON.stringify(stringToList(directions))
+  );
   return (
     <div class="notecard">
       <h1 name="title" {...handlers}>
         {title}
       </h1>
-      <ul name="ingrediants">
+      <ul name="ingrediants" {...handlers}>
         {stringToList(ingrediants).map((s, i) => (
-          <li key={`${i}-${s}`} {...handlers}>
-            {s}
-          </li>
+          <li key={`ingrediant-${i}-${s}`}>{s}</li>
         ))}
       </ul>
-      <ol name="directions">
+      <ol name="directions" {...handlers}>
         {stringToList(directions).map((s, i) => (
-          <li key={`${i}-${s}`} {...handlers}>
-            {s}
-          </li>
+          <li key={`direction-${i}-${s}`}>{s}</li>
         ))}
       </ol>
     </div>
@@ -63,4 +95,16 @@ function stringToList(s: string) {
     .map(s => s.trim())
     .filter(s => s !== "");
   return arr.length === 0 ? [""] : arr;
+}
+
+function listElementToString(el: HTMLElement) {
+  const arr: string[] = [];
+  for (let i = 0; i < el.children.length; i++) {
+    const child = el.children.item(i);
+    arr.push((child as any).innerText);
+  }
+  return arr
+    .map(s => s.trim())
+    .filter(s => s !== "")
+    .join("\n");
 }
