f3ea7c5ada1643be06f888bc07dc3434a8937375
Travis Scholl <traviswscholl@gmail.com>
2020-04-01 17:23:50 -0400

    account for empty cowyo pages

--- src/utils/api_real.ts
+++ src/utils/api_real.ts
@@ -1,18 +1,19 @@
 import { Recipe } from "src/model";
 
 export async function load(page: string): Promise<Array<Recipe>> {
-    const resp = await fetch(`https://cowyo.com/${page}/raw`);
-    const data = await resp.json();
-    return data;
+  const resp = await fetch(`https://cowyo.com/${page}/raw`);
+  const txt = await resp.text();
+  const data = JSON.parse(txt || "[]");
+  return data;
 }
 
 export async function save(page: string, data: Array<Recipe>) {
-  fetch("https://cowyo.com/update",{
-    method: 'POST',
-    mode: 'no-cors',
+  fetch("https://cowyo.com/update", {
+    method: "POST",
+    mode: "no-cors",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({page,new_text:JSON.stringify(data)})
+    body: JSON.stringify({ page, new_text: JSON.stringify(data) })
   });
 }
